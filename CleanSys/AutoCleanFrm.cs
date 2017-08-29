467c1c3bbd4236b57b0c1e9d4103e6eeae6cc9eb
Chenglin Feng <v-chfeng@microsoft.com>
2017-08-29 17:06:42 +0800

    添加 更新用时 label 定时器 和 停止按钮操作

--- CleanSys/AutoCleanFrm.cs
+++ CleanSys/AutoCleanFrm.cs
@@ -1,4 +1,5 @@
 ﻿using CCWin;
+using CCWin.SkinControl;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,12 +10,13 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WinTimer = System.Windows.Forms.Timer;
 
 namespace CleanSys
 {
     public partial class AutoCleanFrm : CCSkinMain
     {
-        private string spendTimeTemplate = @"用时: {0}";
+        private string spendTimeTemplate = @"用时: {0}:{1}";
 
         private string angleOne = "upRight1";
         private string angleTwo = "upRight1";
@@ -38,6 +40,17 @@ namespace CleanSys
         private int stepThreeProcess;
 
         private List<myProcesser> _processList;
+        private List<WinTimer> _timerList;
+        private List<DateTime> _startList;
+        private List<SkinLabel> _spendTextList;
+
+        private bool isWorking;
+
+        private WinTimer timer1;
+        private WinTimer timer2;
+        private WinTimer timer3;
+
+        private Thread thread;
 
         public AutoCleanFrm()
         {
@@ -54,6 +67,43 @@ namespace CleanSys
             this.Location = (Point)myData.myPossition;         //窗体的起始位置为(x,y)
             DataLabel.Text = myData.MiddleTitle();
             TimeLabel.Text = myData.RightTime();
+            this.isWorking = false;
+
+            this.timer1 = new WinTimer();
+            this.timer2 = new WinTimer();
+            this.timer3 = new WinTimer();
+
+            this.timer1.Interval = 1000;
+            this.timer2.Interval = 1000;
+            this.timer3.Interval = 1000;
+
+            this.timer1.Enabled = false;
+            this.timer2.Enabled = false;
+            this.timer3.Enabled = false;
+
+            this.timer1.Tick += new EventHandler(Timer1_Tick);
+            this.timer2.Tick += new EventHandler(Timer2_Tick);
+            this.timer3.Tick += new EventHandler(Timer3_Tick);
+
+            this.InitProcessBar();
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan span = DateTime.Now - this.startTime1;
+            this.spendTime1.Text = string.Format(spendTimeTemplate, span.Minutes, span.Seconds);
+        }
+
+        private void Timer2_Tick(object sender, EventArgs e)
+        {
+            TimeSpan span = DateTime.Now - this.startTime2;
+            this.spendTime2.Text = string.Format(spendTimeTemplate, span.Minutes, span.Seconds);
+        }
+
+        private void Timer3_Tick(object sender, EventArgs e)
+        {
+            TimeSpan span = DateTime.Now - this.startTime3;
+            this.spendTime3.Text = string.Format(spendTimeTemplate, span.Minutes, span.Seconds);
         }
 
         protected void RTC_Tick(object sender, EventArgs e)
@@ -111,23 +161,54 @@ namespace CleanSys
 
         private void AutoClean_Click(object sender, EventArgs e)
         {
-            // 发送 自动清理请求 到 清理机器
-            MachineControler.SendCMD(MachineControler.Command_Start);
-            this.currentMachineNum = 1;
-            this.currentStepNum = 1;
+            if (this.isWorking == false)
+            {
+                // 发送 自动清理请求 到 清理机器
+                bool isSuccess = MachineControler.SendCMD(MachineControler.Command_Start);
+                if (isSuccess)
+                {
+                    this.isWorking = true;
 
-            //
-            DataGet getter = new DataGet();
-            //getter.
+                    this.InitProcessBar();
 
-            // 进度条开始动
+                    this.currentMachineNum = 1;
+                    this.currentStepNum = 1;
 
-            // 灯开始亮
+                    this.startTime1 = DateTime.Now;
+                    this.timer1.Enabled = true;
+                    
+                    DataGet getter = new DataGet();
+                    getter.UpdateUIDelegate += this.Update;
+                    getter.TaskCallBack += this.DoneClean;
+                    this.thread = new Thread(getter.GetStatus);
+                    this.thread.IsBackground = true;
+                    this.thread.Start();
+                }
+            }
+            else
+            {
+                MessageBox.Show("正在清理，请稍后再试");
+            }
         }
 
         private void stopBtn_Click(object sender, EventArgs e)
         {
-            ;
+            DialogResult result = MessageBox.Show("确实终止清理", "确认", MessageBoxButtons.OKCancel);
+            if (result == DialogResult.OK)
+            {
+                bool isSuccess = MachineControler.SendCMD(MachineControler.Command_Stop);
+                if (!isSuccess)
+                {
+                    MessageBox.Show("终止清理失败!");
+                }
+                else
+                {
+                    this.thread.Abort();
+                    this.timer1.Enabled = false;
+                    this.timer2.Enabled = false;
+                    this.timer3.Enabled = false;
+                }
+            }
         }
 
         private void HomeBtn_Click(object sender, EventArgs e)
@@ -156,12 +237,36 @@ namespace CleanSys
             }
         }
 
+        // 重置上次清理痕迹
+        private void DoneClean()
+        {
+            this.isWorking = false;
+        }
+
         private void Update(MachineStatus status)
         {
 
             if (status.IsError)
             {
                 MessageBox.Show(status.ErrorMsg);
+            }
+            else if (status.AllDone)
+            {
+                this.ProcessIncreaseTo(this.process1.ProcessBar, 100);
+                this.ProcessIncreaseTo(this.process2.ProcessBar, 100);
+                this.ProcessIncreaseTo(this.process3.ProcessBar, 100);
+
+                //停止计时
+                this.timer1.Enabled = false;
+                this.timer2.Enabled = false;
+                this.timer3.Enabled = false;
+
+                this.UpdateAngleMachine(4, 3, true);
+
+                // 更新upArrow位置
+                this.UpdateUpArrowLocation(4, 3);
+
+                MessageBox.Show("清理完成");
             }
             else
             {
@@ -173,36 +278,81 @@ namespace CleanSys
                     this.ProcessIncreaseTo(this.process3.ProcessBar, 100);
 
                     //更新用时
+                    this.timer1.Enabled = false;
+                    this.timer2.Enabled = false;
+                    this.timer3.Enabled = false;
 
                     Thread.Sleep(1000);
 
                     this.process1.ProcessBar.Value = 0;
                     this.process2.ProcessBar.Value = 0;
                     this.process3.ProcessBar.Value = 0;
+                    this.spendTime1.Text = string.Format(this.spendTimeTemplate, "00","00");
+                    this.spendTime2.Text = string.Format(this.spendTimeTemplate, "00","00");
+                    this.spendTime3.Text = string.Format(this.spendTimeTemplate, "00","00");
 
-                    //重置用时
                     this.ProcessIncreaseTo(this.process1.ProcessBar, status.stepOneProcess);
                     this.ProcessIncreaseTo(this.process2.ProcessBar, status.stepTwoProcess);
                     this.ProcessIncreaseTo(this.process3.ProcessBar, status.stepThreeProcess);
 
+                    //重置用时
+                    this.StartTimeList[status.stepNum - 1] = DateTime.Now;
+                    this.TimerList[status.stepNum - 1].Enabled = true;
+
+                    // 对于已经完成而没有统计的步骤，写一些假数据。
+                    for (int i = 0; i < status.stepNum - 1; i++)
+                    {
+                        this.SpendTextList[i].Text = string.Format(this.spendTimeTemplate, "00", "30");
+                    }
+
                     this.UpdateAngleMachine(status.machineNum, status.stepNum, true);
+
+                    // 更新upArrow位置
+                    this.UpdateUpArrowLocation(status.machineNum, status.stepNum);
+
+                    // 记录状态
+                    this.currentMachineNum = status.machineNum;
+                    this.currentStepNum = status.stepNum;
                 }// 如果当前轨道的当前步骤完成
                 else if (status.stepNum > this.currentStepNum)
                 {
-                    for (int i = 0; i < status.stepNum - 1; i++)
+                    for (int i = this.currentStepNum - 1; i < status.stepNum - 1; i++)
                     {
                         this.ProcessIncreaseTo(this.ProcessList[i].ProcessBar, 100);
+                        this.TimerList[i].Enabled = false;
                     }
+
+                    this.StartTimeList[status.stepNum - 1] = DateTime.Now;
+                    this.TimerList[status.stepNum - 1].Enabled = true;
 
                     this.UpdateAngleMachine(status.machineNum, status.stepNum);
 
                     this.ProcessIncreaseTo(this.ProcessList[status.stepNum - 1].ProcessBar, status.StepsList[status.stepNum - 1]);
+
+                    // 更新upArrow位置
+                    this.UpdateUpArrowLocation(status.machineNum, status.stepNum);
+
+                    // 记录状态
+                    this.currentStepNum = status.stepNum;
                 }
                 else
                 {
                     this.ProcessIncreaseTo(this.ProcessList[status.stepNum - 1].ProcessBar, status.StepsList[status.stepNum - 1]);
                 }
             }
+        }
+
+        private void UpdateUpArrowLocation(int num, int step)
+        {
+            int left = -2;
+            int top = 301;
+
+            int increase = 25;
+            int rate = (num - 1) * 3 + step;
+            top -= increase * rate;
+
+            System.Drawing.Point newLocation = new Point(left, top);
+            this.upArrow.Location = newLocation;
         }
 
         /// <summary>
@@ -290,6 +440,55 @@ namespace CleanSys
                 }
 
                 return this._processList;
+            }
+        }
+
+        public List<WinTimer> TimerList
+        {
+            get
+            {
+                if (this._timerList == null)
+                {
+                    this._timerList = new List<WinTimer>();
+                    this._timerList.Add(this.timer1);
+                    this._timerList.Add(this.timer2);
+                    this._timerList.Add(this.timer3);
+                }
+
+                return this._timerList;
+            }
+        }
+
+        public List<DateTime> StartTimeList
+        {
+            get
+            {
+                if (this._startList == null)
+                {
+                    this._startList = new List<DateTime>();
+                    this._startList.Add(this.startTime1);
+                    this._startList.Add(this.startTime2);
+                    this._startList.Add(this.startTime3);
+                }
+
+                //this.sp
+                return this._startList;
+            }
+        }
+
+        public List<CCWin.SkinControl.SkinLabel> SpendTextList
+        {
+            get
+            {
+                if (this._spendTextList == null)
+                {
+                    this._spendTextList = new List<SkinLabel>();
+                    this._spendTextList.Add(this.spendTime1);
+                    this._spendTextList.Add(this.spendTime2);
+                    this._spendTextList.Add(this.spendTime3);
+                }
+
+                return this._spendTextList;
             }
         }
     }
